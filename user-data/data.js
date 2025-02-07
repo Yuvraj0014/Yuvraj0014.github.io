df8c4f9b4a7d64f774250d0db9930deab8db61ba
Yuvraj0014 <yuvraj34275@gmail.com>
2025-02-07 14:03:31 +0530

    uploading files

---
+++ user-data/data.js
@@ -0,0 +1,132 @@
+export const bio = [
+  "<strong>Welcome to my portfolio! I'm Yuvrsj Muthyala.</strong>",
+  "<strong>Driven to unlock insights as a Data Science & AI/ML Engineer enthusiast</strong>", 
+  "With a passion for understanding complex systems, I've found my calling in data science, where I can unravel patterns and uncover meaningful connections.",
+  "I'm excited to leverage data-driven approaches to drive decision-making and solve real-world problems, and I'm always on the lookout for innovative projects to collaborate on.",
+  "Proficient in a range of data science disciplines, including Machine Learning, Deep Learning, NLP, and GenAI, with a solid grasp of statistical concepts and programming fundamentals.",
+  "Dedicated to fostering a culture of knowledge-sharing, collaboration, and continuous learning, with a focus on mentoring, teamwork, and process improvement.",
+];
+
+export const skills = [
+  "Python, Data Analysis & Manipulation, Machine Learning, Deep Learning, NLP, AI/GenAI",
+  "HuggingFace, Langchain, LLM's, GitHub, Streamlit", 
+  "Numpy/Pandas, Matplotlib/Seaborn, Sklearn, Tensorflow/Keras, NLTK, Transformers",
+];
+
+export const experience = [
+  {
+    title: "Embrizon Technologies",
+    // duration: "Internship Duration",
+    subtitle: "Data Science Intern",
+    details: [
+      "Received comprehensive training on Python, machine learning, and deep learning, which covered topics like data preprocessing, feature engineering, model selection, and hyperparameter tuning.",
+      "Worked on a project to detect Capuchin bird calls using spectrogram analysis and convolutional neural networks (CNNs), which involved collecting and preprocessing a dataset of bird calls, selecting relevant features, training the model, and evaluating its performance using metrics like accuracy, precision, and recall.",
+      "Successfully completed the project and received a project completion certificate, as well as a training completion certificate and an internship completion certificate.",
+      "Received a completion report that highlighted the key achievements and outcomes of the project, and provided recommendations for future improvements.",
+    ],
+    tags: ["Data Science", "Machine Learning", "Deep Learning", "Spectrogram Analysis", "CNN"],
+    icon: "bird",
+  },
+  {
+    title: "InternPE",
+    // duration: "Internship Duration",
+    subtitle: "AI/ML Intern",
+    details: [
+      "Designed and developed a machine learning model for diabetes prediction, which involved collecting and preprocessing a dataset of health records, selecting relevant features, training a supervised learning algorithm, and evaluating the model's performance using metrics like accuracy, precision, and recall.",
+      "Developed a car price prediction model using a regression algorithm, which required gathering a dataset of car features and prices, preprocessing the data, selecting the most relevant features, training the model, and evaluating its performance using metrics like mean squared error and R-squared.",
+      "Created a predictive model for IPL winning team prediction, which involved collecting and preprocessing a dataset of historical match data, selecting relevant features, training a classification algorithm, and evaluating the model's performance using metrics like accuracy, precision, and recall.",
+      "Designed and developed a deep learning model for breast cancer detection, which involved collecting and preprocessing a dataset of mammography images, selecting relevant features, training a convolutional neural network (CNN), and evaluating the model's performance using metrics like accuracy, precision, and recall.",
+      "Optimized the performance of the models through techniques like feature engineering, hyperparameter tuning, and ensemble methods, and deployed them in a simulated environment to make predictions on new data.",
+    ],
+    tags: ["Machine Learning", "Deep Learning", "Data Preprocessing", "Data Visualization", "Model Deployment", "AI"],
+    icon: "robot",
+  },
+];
+
+export const education = [
+  {
+    title: "M.Sc. in Data Science",
+    duration: "2022 - 2024",
+    subtitle: "Loyola Academy, Hyderabad",
+    details: ["CGPA: 9.09"],
+    tags: [
+      "Statistics/Mathematics",
+      "Machine Learning",
+      "Deep Learning",
+      "Artificial Intelligence",
+      "Python",
+    ],
+    icon: "graduation-cap",
+  },
+  {
+    title: "B.Sc. in Electronics and Communication Technology",
+    duration: "2019 - 2022",
+    subtitle: "Loyola Academy, Hyderabad",
+    details: ["CGPA: 8.24"],
+    tags: ["Mathematics", "MicroProcessor & Controller", "Internet of Things", "Data Communication & Networking", "Computer Science"],
+    icon: "book",
+  },
+];
+
+export const footer = [
+  //   {
+  //     label: "Dev Profiles",
+  //     data: [
+  //       {
+  //         text: "Stackoverflow",
+  //         link: "https://stackoverflow.com",
+  //       },
+  //       {
+  //         text: "GitHub",
+  //         link: "https://github.com/kiran-91",
+  //       },
+  //       {
+  //         text: "LeetCode",
+  //         link: "https://leetcode.com",
+  //       },
+  //     ],
+  //   },
+     {
+       label: "Resources",
+       data: [
+           //{
+           //text: "Enable Dark/Light Mode",
+           //func: "enableDarkMode()",
+         //},
+         {
+           text: "Print this page",
+           func: "window.print()",
+         },
+        ],
+        },
+  //         text: "Clone this page",
+  //         link: "https://github.com/",
+  //       },
+  //     ],
+  //   },
+  {
+    label: "My Profiles",
+    data: [
+         {
+           text: "Linkedin",
+           link: "https://www.linkedin.com/in/yuvrajmuthyala/",
+         },
+      //   {
+      //     text: "Twitter",
+      //     link: "https://twitter.com/thesigmakid",
+      //   },
+      //{
+      //  text: "Buy me a coffee",
+      //  link: "https://www.buymeacoffee.com/r194dME8y",
+      //},
+      {
+        text: "Github",
+        link: "https://github.com/Yuvraj0014",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: ["Made with &hearts; by Yuvraj Muthyala"],
+  },
+];
